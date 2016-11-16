c7870749f99ed1db22f69da6b4e1d457be19bb94
adam bresnen <abresnen18@VenerableBede.schools.sunderland.gov.uk>
2016-11-16 12:11:59 +0000

    My cool game and adventure!

--- quiz.py
+++ quiz.py
@@ -1,8 +1,86 @@
 # Our quiz!
 
-def quiz():
+score = 0
+name = ""
 
-    ... make a quiz!
+def quiz():
+    global name
+
+    name = input("Name: ")
+
+    question1()
+    question2()
+    question3()
+
+    replay = input("Try again (Y/N): ")
+    if replay.lower() == "y":
+        quiz()
+    
+
+  
+def question1():
+    global score
+    global name
+
+    print("So", name, "which football player won the Ballon d'Or last year?")
+    answer = input("Answer: ")
+
+    if answer.lower() in "cristiano ronaldo":
+        print("Correct!")
+        score = score + 1
+
+    else:
+        print("Wrong!")
+
+    
+
+def question2():
+    global score
+    global name
+
+    print("Now", name, "which team won the Premier League last season?")
+    answer = input("Answer: ")
+
+    if answer.lower() in "leicester":
+        print("Correct!")
+        score = score + 1
+
+    else:
+        print("Wrong!")
+
+   
+
+def question3():
+    global score
+    global name
+
+    print("Finally, name one of the three teams who were relegated last season", name + ":")
+    answer = input("Answer: ")
+
+    if answer.lower() in "astonvillanewcastlenorwich":
+        print("Correct!")
+        score = score + 1
+
+    else:
+        print("Wrong!")
+
+    if score == 3:
+        print("Congratulations! You are a football expert!")
+
+    elif score == 2:
+        print("Good job! You know your stuff!")
+
+    elif score == 1:
+        print("Not great! You should watch more!")
+
+    else:
+        print("I think you are on the wrong quiz!")
+
+
+
+    
+
+  
 
 
 
